25f19b0fad14bed7e8b2f3aa37ce405a483b4e12
y2336789@naver.com <63240477+y2336789@users.noreply.github.com>
2022-05-15 15:36:13 +0900

    05.15

---
+++ 왕실의 나이트.py
@@ -0,0 +1,15 @@
+input_data = input()
+
+row = int(input_data[1])
+column = ord(input_data[0]) - ord('a') + 1
+
+steps = [(-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2)]
+
+count = 0
+for step in steps:
+    new_row = row + step[0]
+    new_column = column + step[1]
+    if new_row >= 1 and new_row <= 8 and new_column >=1 and new_column <= 8 :
+        count += 1
+
+print(count)
